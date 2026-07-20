ee35cd49f6477454fc26c92542a5c23838eddbd4
diegoselingardi <diego.selingardi@gmail.com>
2026-07-20 00:33:54 -0300

    Corringindo clique no teclado virtual novamente

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "termeeple-v1";
+const CACHE_NAME = "termeeple-v2";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -12,6 +12,18 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       return cached || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)) 
+      );
     })
   );
 });
